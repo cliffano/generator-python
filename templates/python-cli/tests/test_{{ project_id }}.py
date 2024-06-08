b4207a7de937666c4c10ffca172b1df5afb6fd7f
Cliffano Subagio <cliffano@gmail.com>
2024-06-09 00:37:39 +1000

    Add test scenario for non-default args.

--- templates/python-cli/tests/test_{{ project_id }}.py
+++ templates/python-cli/tests/test_{{ project_id }}.py
@@ -24,6 +24,22 @@ class Test{{ project_name }}(unittest.TestCase):
 
         display(conf_file='{{ project_id }}.yaml', reverse=False, transformation='lower')
 
+    @patch('{{ project_id }}.load')
+    @patch('{{ project_id }}.init')
+    def test_display_with_non_default_args( # pylint: disable=too-many-arguments
+            self,
+            func_init,
+            func_load):
+
+        mock_logger = unittest.mock.Mock()
+
+        func_init.return_value = mock_logger
+        func_load.return_value = {
+            'text': 'Hello World'
+        }
+
+        display(conf_file='{{ project_id }}.yaml', reverse=True, transformation='upper')
+
     @patch('{{ project_id }}.display')
     def test_cli( # pylint: disable=too-many-arguments
             self,

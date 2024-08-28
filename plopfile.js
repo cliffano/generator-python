c219eb82d30fc018df10a4f0f2cad2aca9552a5e
Cliffano Subagio <cliffano@gmail.com>
2024-08-28 22:39:18 +1000

    Add github_repo param to python-lib module.

--- plopfile.js
+++ plopfile.js
@@ -97,6 +97,11 @@ export default function (plop) {
         type: 'input',
         name: 'github_id',
         message: 'GitHub ID'
+      },
+      {
+        type: 'input',
+        name: 'github_repo',
+        message: 'GitHub Repository'
       }
     ],
     actions: [

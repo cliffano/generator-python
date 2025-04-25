a2740340155304f2cc3989d6feb209b70091bb41
Cliffano Subagio <cliffano@gmail.com>
2025-04-25 13:39:27 +1000

    Switch example section log to use printf.

--- templates/python-cli/examples/{{project_id}}-cli.sh
+++ templates/python-cli/examples/{{project_id}}-cli.sh
@@ -4,20 +4,18 @@ set -o nounset
 cd ../
 . ./.venv/bin/activate
 
-echo "\n\n========================================"
-echo "Show help guide: {{project_id}} --help"
+printf "\n\n========================================\n"
+printf "Show help guide:\n"
 {{project_id}} --help
 
-echo "\n\n========================================"
-echo "Run command with default config file: {{project_id}}"
+printf "\n\n========================================\n"
+printf "Run command with default config file:\n"
 {{project_id}}
 
-echo "\n\n========================================"
-echo "Run command with specified config file:"
-echo "{{project_id}} --conf-file {{project_id}}.yaml"
+printf "\n\n========================================\n"
+printf "Run command with specified config file:\n"
 {{project_id}} --conf-file {{project_id}}.yaml
 
-echo "\n\n========================================"
-echo "Run command with specified config file and custom flags:"
-echo "{{project_id}} --conf-file {{project_id}}.yaml --reverse --transformation upper"
+printf "\n\n========================================\n"
+printf "Run command with specified config file and custom flags:\n"
 {{project_id}} --conf-file {{project_id}}.yaml --reverse --transformation upper

b8cb5d17653fe4109bbeec09104a1262fbf44b82
Cliffano Subagio <cliffano@gmail.com>
2026-07-26 23:29:28 +1000

    Add missing input author_url.

--- plopfile.js
+++ plopfile.js
@@ -88,6 +88,11 @@ export default function (plop) {
     },
     {
       type: 'input',
+      name: 'author_url',
+      message: 'Author URL'
+    },
+    {
+      type: 'input',
       name: 'github_id',
       message: 'GitHub ID'
     },

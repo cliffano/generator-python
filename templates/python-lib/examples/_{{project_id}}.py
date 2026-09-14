8b766d4cf37200731373d7b5d253fdbe4188a87e
Cliffano Subagio <cliffano@gmail.com>
2026-09-14 16:42:36 +1000

    Update main and template workflows to use save-changes-action on deps upgrade.

--- templates/python-lib/examples/_{{project_id}}.py
+++ templates/python-lib/examples/_{{project_id}}.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-module-docstring,missing-class-docstring,missing-function-docstring,duplicate-code,too-many-locals
 from {{project_id}} import Display
 
-display = Display(conf_files=['{{project_id}}.yaml'])
+display = Display(conf_file='{{project_id}}.yaml')
 text = display.format(reverse=False, transformation='lower')
 print(text)
